89635ca14607049b10c64d8d05e5135df59f782d
丁一鸣 <315129552@qq.com>
2015-11-15 23:41:24 +0800

    添加api准备使用ajax进行唯一性判断

--- app/Api/Controllers/ValidateController.php
+++ app/Api/Controllers/ValidateController.php
@@ -10,8 +10,29 @@ class ValidateController extends BaseController
     public function onephone(Request $request)
     {
         //$phone = $request->input('phone');
+        // $res = Userinfo::where('phone',$phone)->get();
+        // if($res)
+        // {
+        //     //存在一样的手机号
+        // }else{
+        //     //不存在一样的手机号
+        //
+        // }
         return Userinfo::all();
 
+    }
+
+    public function oneidentity(Request $request)
+    {
+        $identity = $request->input('identity');
+        $res = Userinfo::where('identity',$identity)->get();
+        if($res)
+        {
+            //存在同样地身份证号
+
+        }else{
+            //不存在同样地身份证号
+        }
     }
 
 
