7b53cb8c05fb31538a30cdc92f3114aa9821ac41
丁一鸣 <315129552@qq.com>
2015-11-16 11:28:35 +0800

    编辑信息页面验证

--- resources/views/userinfo/edit.blade.php
+++ resources/views/userinfo/edit.blade.php
@@ -5,11 +5,15 @@
 @endsection
 
 @section('content')
-    <div class="container create-form shadow-z-1">
+    <div class="container create-form shadow-z-1" id="app">
         <form v-form name="myform" @submit.prevent="onSubmit" method="post" action="/userinfo/'{{$userinfo->id}}"
               id="myform">
             <input name="_method" type="hidden" value="PATCH">
             @include('common.form',['form_title' => '编辑用户信息'])
         </form>
     </div>
+@endsection
+@section('js')
+    <script src="{{asset('assets/vuejs/vue-form.min.js')}}"></script>
+    <script src="{{asset('assets/vuejs/userinfo_form_validate.js')}}"></script>
 @endsection
