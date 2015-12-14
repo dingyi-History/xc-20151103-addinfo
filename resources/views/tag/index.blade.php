d92d14a55fba2286d1aecc3fe3a452b816ca3dad
丁一鸣 <315129552@qq.com>
2015-12-14 11:35:30 +0800

    修缮

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -2,11 +2,24 @@
 @section('content')
     <div class="am-g">
         <div class="border-1 am-animation-slide-left">
-            @include('base.title',['title' => '全部标签'])
+            <div class="title am-primary ">
+                <h2 class="am-fl">全部标签</h2>
+                <div class="am-fr">
+                    <input type="button" class="am-btn am-btn-warning " value="新建标签" id="tagbtn"/>
+                </div>
+                <div class="am-fr">
+                    <input type="text" id="addtag" class="am-form-field" style="display: inline-block;" >
+                </div>
+
+            </div>
+
             @foreach($taglist as $tag)
                 <a href="/tag/show/{{$tag->id}}"><span class="am-badge am-badge-success am-round am-text-lg"
                                                        style="font-size: 18px;margin-bottom: 15px;">{{$tag->name}}</span></a>
             @endforeach
         </div>
     </div>
+@endsection
+@section('js')
+    @include('base.tag')
 @endsection
